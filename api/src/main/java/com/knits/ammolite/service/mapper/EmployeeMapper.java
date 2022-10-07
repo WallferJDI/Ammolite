ef8c9fbcb8b62d7d3513f44b2dd4eb4f381b60d6
Atanu Biswas <82307753+Atanu484@users.noreply.github.com>
2022-10-08 01:36:58 +0300

    Removing INSTANCE

--- api/src/main/java/com/knits/ammolite/service/mapper/EmployeeMapper.java
+++ api/src/main/java/com/knits/ammolite/service/mapper/EmployeeMapper.java
@@ -1,14 +1,10 @@
 package com.knits.ammolite.service.mapper;
 
 import com.knits.ammolite.model.employee.Employee;
-import com.knits.ammolite.service.dto.EmployeeDto;
+import com.knits.ammolite.service.dto.employee.EmployeeDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 @Mapper(componentModel = "spring")
 public interface EmployeeMapper {
-
-    EmployeeMapper INSTANCE = Mappers.getMapper(EmployeeMapper.class);
-
     EmployeeDto modelEmployeeToEmployeeDto(Employee employee);
     Employee employeeDtoToModelEmployee(EmployeeDto employeeDto);
 }
