b873a06015f9c3dab1a8ced50ee3fbb0d03cb134
Atanu Biswas <82307753+Atanu484@users.noreply.github.com>
2022-10-10 03:47:03 +0300

    Update Employee.java

--- api/src/main/java/com/knits/ammolite/model/Employee.java
+++ api/src/main/java/com/knits/ammolite/model/Employee.java
@@ -1,30 +1,42 @@
 package com.knits.ammolite.model.employee;
 
 import lombok.Data;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+import org.hibernate.annotations.SQLDelete;
 
 import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 @Data
 @Table(name = "employee")
-public class Employee {
+@SQLDelete(sql = "UPDATE table_product SET deleted = true WHERE id=?")
+@FilterDef(name = "deletedEmployeeFilter", parameters = @ParamDef(name = "isDeleted", type = "boolean"))
+@Filter(name = "deletedEmployeeFilter", condition = "deleted = :isDeleted")
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
     @SequenceGenerator(name = "sequenceGenerator")
     private Long id;
 
-    @Column(name = "first_name", length = 50)
+    @Column(name = "first_name", length = 50, nullable = false)
     private String firstName;
 
-    @Column(name = "last_name", length = 50)
+    @Column(name = "last_name", length = 50, nullable = false)
     private String lastName;
 
-    @Column(name = "email", length = 254, unique = true)
+    @Column(name = "email", length = 254, unique = true, nullable = false)
     private String email = firstName + "." + lastName + "@kuehne-nagel.com";
 
-    @Column(name = "date_of_birth")
+    @Column(name = "date_of_birth", nullable = false)
     private Date dateOfBarth;
 
     @Enumerated(EnumType.STRING)
@@ -40,28 +52,39 @@ public class Employee {
     private String companyMobileNumber;
 
     @Column(name = "role")
+    @Embedded
     private Role role;
 
     @Column(name = "business_unit")
+    @Embedded
     private BusinessUnit businessUnit;
 
     @Column(name = "organization")
-    private Organizaion organizaion;
+    @Embedded
+    private Organization organization;
 
     @Column(name = "office")
+    @Embedded
     private Office office;
 
     @Column(name = "job_title")
+    @Embedded
     private JobTitle jobTitle;
 
     @Column(name = "department")
+    @Embedded
     private Department department;
 
     @Column(name = "division")
+    @Embedded
     private Division division;
 
     @Column(name = "solid_line_manager")
+    @Embedded
     private SolidLineManager solidLineManager;
+
+    @Column(name = "deleted", nullable = false)
+    private boolean deleted = Boolean.FALSE;
 }
 
 
