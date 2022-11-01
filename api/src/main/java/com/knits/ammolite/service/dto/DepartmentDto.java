d49a3ef3d712065d48727ea25b04c7941e34a6f5
Atanu Biswas <82307753+Atanu484@users.noreply.github.com>
2022-11-02 00:33:28 +0200

    Add files via upload

---
+++ api/src/main/java/com/knits/ammolite/service/dto/DepartmentDto.java
@@ -0,0 +1,17 @@
+package com.knits.ammolite.service.dto.employee;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class DepartmentDto {
+    private Long id;
+    @NotEmpty
+    private String name;
+}
+
---
+++ api/src/main/java/com/knits/ammolite/service/dto/DivisionDto.java
@@ -0,0 +1,16 @@
+package com.knits.ammolite.service.dto.employee;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class DivisionDto {
+    private Long id;
+    @NotEmpty
+    private String name;
+}
